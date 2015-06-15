01045070714778c35b3a59ff6ec2a41e925c1a16
Justin <rssnsj@gmail.com>
2015-06-15 23:33:43 +0800

    Fix incorrect buffer size in 'struct minivtun_msg' that causes crash.

--- src/minivtun.h
+++ src/minivtun.h
@@ -15,6 +15,8 @@ enum {
 	MINIVTUN_MSG_DISCONNECT,
 };
 
+#define NM_PI_BUFFER_SIZE  (1024 * 8)
+
 struct minivtun_msg {
 	struct {
 		__u8 opcode;
@@ -26,7 +28,7 @@ struct minivtun_msg {
 		struct {
 			__be16 proto;   /* ETH_P_IP or ETH_P_IPV6 */
 			__be16 ip_dlen; /* Total length of IP/IPv6 data */
-			char data[1024 * 2];
+			char data[NM_PI_BUFFER_SIZE];
 		} __attribute__((packed)) ipdata;
 		struct {
 			struct in_addr loc_tun_in;
@@ -37,8 +39,6 @@ struct minivtun_msg {
 
 #define MINIVTUN_MSG_BASIC_HLEN (sizeof(((struct minivtun_msg *)0)->hdr))
 #define MINIVTUN_MSG_IPDATA_OFFSET (offsetof(struct minivtun_msg, ipdata.data))
-
-#define NM_PI_BUFFER_SIZE  (1024 * 8)
 
 extern unsigned g_reconnect_timeo;
 extern unsigned g_keepalive_timeo;
